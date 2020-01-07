957d8c69cf383208e27b1b292b5bba4fd90c2f51
Alli <57212624+allibateman16@users.noreply.github.com>
2020-01-07 18:02:26 +0100

    basic content commit

---
+++ basic-content/sub-pages/foodsharing/script.js
@@ -0,0 +1,56 @@
+
+function scrollto(x, y) {
+    window.scrollTo(x, y);
+}
+
+document.getElementById("video").innerHTML = "<div id='player'></div>";
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '500',
+        width: window.innerWidth,
+        videoId: '-Lf-bfXFTc4',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    //                event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        document.getElementById("videoMenuDiv").style.animation = 'videoMenuFadeout 2s normal forwards';
+        document.getElementById("videoMenuDiv").style.pointerEvents = "none";
+    }
+    if (event.data == YT.PlayerState.PAUSED) {
+        document.getElementById("videoMenuDiv").style.animation = 'videoMenuFadein 2s normal forwards';
+        document.getElementById("videoMenuDiv").style.pointerEvents = "auto";
+
+
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
